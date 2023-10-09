225a16f1980d3f6ca2ca11e3651ab8d74847a543
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2023-10-09 08:56:22 +0000

    added the data for daily

---
+++ script.js
@@ -0,0 +1,127 @@
+
+const Jsondata =[
+  {
+    "title": "Work",
+    "timeframes": {
+      "daily": {
+        "current": 5,
+        "previous": 7
+      },
+      "weekly": {
+        "current": 32,
+        "previous": 36
+      },
+      "monthly": {
+        "current": 103,
+        "previous": 128
+      }
+    }
+  },
+  {
+    "title": "Play",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 2
+      },
+      "weekly": {
+        "current": 10,
+        "previous": 8
+      },
+      "monthly": {
+        "current": 23,
+        "previous": 29
+      }
+    }
+  },
+  {
+    "title": "Study",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 7
+      },
+      "monthly": {
+        "current": 13,
+        "previous": 19
+      }
+    }
+  },
+  {
+    "title": "Exercise",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 5
+      },
+      "monthly": {
+        "current": 11,
+        "previous": 18
+      }
+    }
+  },
+  {
+    "title": "Social",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 3
+      },
+      "weekly": {
+        "current": 5,
+        "previous": 10
+      },
+      "monthly": {
+        "current": 21,
+        "previous": 23
+      }
+    }
+  },
+  {
+    "title": "Self Care",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 2,
+        "previous": 2
+      },
+      "monthly": {
+        "current": 7,
+        "previous": 11
+      }
+    }
+  }
+]
+
+
+function updateHTML() {
+  const mainActivities = document.querySelectorAll('.main-activity');
+
+  mainActivities.forEach(activity => {
+    const title = activity.querySelector('h4');
+    const currentHours = activity.querySelector('h3');
+    const previousHours = activity.querySelector('p');
+
+    const activityData = Jsondata.find(item => item.title.toLowerCase() === title.textContent.toLowerCase());
+
+    if (activityData) {
+      currentHours.textContent = activityData.timeframes.daily
+    .current + 'hrs';
+      previousHours.textContent = `Yesterday-${activityData.timeframes.daily.previous}hrs`;
+    }
+  });
+}
+
+
+updateHTML();
