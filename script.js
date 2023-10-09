64dba2fec0ce4f83f388aab309ebd7aa47b24ed6
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2023-10-09 13:33:29 +0000

    updated js

--- script.js
+++ script.js
@@ -109,11 +109,10 @@ let daily = document.querySelector(".daily");
 let weekly = document.querySelector(".weekly");
 let monthly = document.querySelector(".monthly");
 
-
+window.onload = DailyFunction();
 daily.addEventListener('click', DailyFunction);
 weekly.addEventListener('click', WeeklyFunction);
 monthly.addEventListener('click', MonthlyFunction);
-window.onload = WeeklyFunction();
 
 function DailyFunction(){
   daily.classList.add('active');
