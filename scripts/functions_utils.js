7bf27369dd980b5eb3ed27aa012b3caeaa3a60a6
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2024-02-02 12:55:17 +0000

    updated the daily,weekly, monthly function

--- scripts/functions_utils.js
+++ scripts/functions_utils.js
@@ -5,64 +5,40 @@ export let monthly = document.querySelector(".monthly");
 import { jsonData } from "./script.js";
 
 
-export function DailyFunction(){
+export function DailyFunction() {
   daily.classList.add('active');
   monthly.classList.remove('active');
   weekly.classList.remove('active');
-  
-  let contentContainers = document.querySelectorAll(".main-activity .content");
-  contentContainers[0].innerHTML = `<h3>${jsonData[0].timeframes.daily.current + 'hrs'} </h3> 
-    <p>${'Yesterday - ' + jsonData[0].timeframes.daily.previous + 'hrs'} </p> `;
-  contentContainers[1].innerHTML=` <h3>${jsonData[1].timeframes.daily.current + 'hr'} </h3> 
-  <p>${'Yesterday - '+ jsonData[1].timeframes.daily.previous + 'hrs'} </p> `
-  contentContainers[2].innerHTML=` <h3>${jsonData[2].timeframes.daily.current + 'hr'} </h3> 
-  <p>${'Yesterday - '+ jsonData[2].timeframes.daily.previous + 'hr'} </p> `
-  contentContainers[3].innerHTML=` <h3>${jsonData[3].timeframes.daily.current + 'hr'} </h3> 
-  <p>${'Yesterday - '+ jsonData[3].timeframes.daily.previous + 'hr'} </p> `
-  contentContainers[4].innerHTML=` <h3>${jsonData[4].timeframes.daily.current + 'hr'} </h3> 
-  <p>${'Yesterday - '+ jsonData[4].timeframes.daily.previous + 'hrs'} </p> `
-  contentContainers[5].innerHTML=` <h3>${jsonData[5].timeframes.daily.current + 'hr'} </h3> 
-  <p>${'Yesterday - '+ jsonData[5].timeframes.daily.previous + 'hr'} </p> `
-}
 
-export function WeeklyFunction(){
+  let contentContainers = document.querySelectorAll(".main-activity .content");
+
+  jsonData.forEach((item, index) => {
+      contentContainers[index].innerHTML = `<h3>${item.timeframes.daily.current + 'hrs'} </h3> 
+          <p>${'Yesterday - ' + item.timeframes.daily.previous + 'hrs'} </p>`;
+  });
+}
+export function WeeklyFunction() {
   daily.classList.remove('active');
   monthly.classList.remove('active');
   weekly.classList.add('active');
 
   let contentContainers = document.querySelectorAll(".main-activity .content");
-  contentContainers[0].innerHTML=` <h3>${jsonData[0].timeframes.weekly.current + 'hrs'} </h3> 
-  <p>${'Last Week - '+ jsonData[0].timeframes.weekly.previous + 'hrs'} </p> `
-  contentContainers[1].innerHTML=` <h3>${jsonData[1].timeframes.weekly.current + 'hrs'} </h3> 
-  <p>${'Last Week - '+ jsonData[1].timeframes.weekly.previous + 'hrs'} </p> `
-  contentContainers[2].innerHTML=` <h3>${jsonData[2].timeframes.weekly.current + 'hrs'} </h3> 
-  <p>${'Last Week - '+ jsonData[2].timeframes.weekly.previous + 'hrs'} </p> `
-  contentContainers[3].innerHTML=` <h3>${jsonData[3].timeframes.weekly.current + 'hrs'} </h3> 
-  <p>${'Last Week - '+ jsonData[3].timeframes.weekly.previous + 'hrs'} </p> `
-  contentContainers[4].innerHTML=` <h3>${jsonData[4].timeframes.weekly.current + 'hrs'} </h3> 
-  <p>${'Last Week - '+ jsonData[4].timeframes.weekly.previous + 'hrs'} </p> `
-  contentContainers[5].innerHTML=` <h3>${jsonData[5].timeframes.weekly.current + 'hrs'} </h3> 
-  <p>${'Last Week - '+ jsonData[5].timeframes.weekly.previous + 'hrs'} </p> `
+  jsonData.forEach((item, index) => {
+      contentContainers[index].innerHTML = `<h3>${item.timeframes.weekly.current + 'hrs'} </h3> 
+          <p>${'Last Week - ' + item.timeframes.weekly.previous + 'hrs'} </p>`;
+  });
 }
 
-export function MonthlyFunction(){
+export function MonthlyFunction() {
   daily.classList.remove('active');
   monthly.classList.add('active');
   weekly.classList.remove('active');
 
   let contentContainers = document.querySelectorAll(".main-activity .content");
-  contentContainers[0].innerHTML=` <h3>${jsonData[0].timeframes.monthly.current + 'hrs'} </h3> 
-  <p>${'Last Month - '+ jsonData[0].timeframes.monthly.previous + 'hrs'} </p> `
-  contentContainers[1].innerHTML=` <h3>${jsonData[1].timeframes.monthly.current + 'hrs'} </h3> 
-  <p>${'Last Month - '+ jsonData[1].timeframes.monthly.previous + 'hrs'} </p> `
-  contentContainers[2].innerHTML=` <h3>${jsonData[2].timeframes.monthly.current + 'hrs'} </h3> 
-  <p>${'Last Month - '+ jsonData[2].timeframes.monthly.previous + 'hrs'} </p> `
-  contentContainers[3].innerHTML=` <h3>${jsonData[3].timeframes.monthly.current + 'hrs'} </h3> 
-  <p>${'Last Month - '+ jsonData[3].timeframes.monthly.previous + 'hrs'} </p> `
-  contentContainers[4].innerHTML=` <h3>${jsonData[4].timeframes.monthly.current + 'hrs'} </h3> 
-  <p>${'Last Month - '+ jsonData[4].timeframes.monthly.previous + 'hrs'} </p> `
-  contentContainers[5].innerHTML=` <h3>${jsonData[5].timeframes.monthly.current + 'hrs'} </h3> 
-  <p>${'Last Month - '+ jsonData[5].timeframes.monthly.previous + 'hrs'} </p> `
-}
 
+  jsonData.forEach((item, index) => {
+      contentContainers[index].innerHTML = `<h3>${item.timeframes.monthly.current + 'hrs'} </h3> 
+          <p>${'Last Month - ' + item.timeframes.monthly.previous + 'hrs'} </p>`;
+  });
+}
 
